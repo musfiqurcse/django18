24a592149a9ec3ef0655e92c8f7e4726c419e1c6
musfiqurcse <musfiqur.rahman@bs-23.net>
2017-05-26 09:08:59 +0600

    tutorial 13 complete till  django form validation

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .form import SignUpForm
 from .models import SignUp
-admin.site.register(SignUp)
+
+class SignUpAdmin(admin.ModelAdmin):
+    list_display=["__str__","timestamp",'updated']
+    form=SignUpForm
+    # class Meta:
+    #     model = SignUp
+admin.site.register(SignUp,SignUpAdmin)
---
+++ src/newsletter/form.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import SignUp
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model=SignUp
+        fields=['full_name','email']
+        # exclude=['full_name']
+
+    #validation for email address
+    def clean_email(self):
+        email=self.cleaned_data.get('email')
+        email_base,provider=email.split('@')
+        domain,extension=provider.split('.')
+        if not extension=='edu':
+            raise forms.ValidationError('Please enter valid email address')
+        return email
+
+
